5db6dcbbdba53bed35e5fea8d2c7e72205d7495f
Martin Goulet <martin.goulet@live.ca>
2018-04-19 21:54:35 -0400

    Correction Layout

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './composantes/navigation/navigation.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { RapportComponent } from './pages/rapport/rapport.component';
 
+const appRoutes: Routes = [
+  { path: 'rapport', component: RapportComponent},
+  { path: 'rapport2', component: RapportComponent},
+  { path: '',
+    redirectTo: '/rapport',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavbarComponent,
+    SidebarComponent,
+    RapportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } 
+    )
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/pages/rapport/rapport.component.spec.ts
+++ ClientApp/src/app/pages/rapport/rapport.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavigationComponent } from './navigation.component';
+import { RapportComponent } from './rapport.component';
 
-describe('NavigationComponent', () => {
-  let component: NavigationComponent;
-  let fixture: ComponentFixture<NavigationComponent>;
+describe('RapportComponent', () => {
+  let component: RapportComponent;
+  let fixture: ComponentFixture<RapportComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
+      declarations: [ RapportComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationComponent);
+    fixture = TestBed.createComponent(RapportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
